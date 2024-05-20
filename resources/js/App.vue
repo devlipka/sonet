5fd591bcf1db8874e3af0e0dcb6ec718220b1216
devlipka <alanneck13@gmail.com>
2024-05-21 00:10:52 +0300

    Install FE dependencies, add prettier and eslint, configure SPA

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="index">Index</router-link>
+        <router-link to="page">Page</router-link>
+        <router-view></router-view>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style scoped></style>
